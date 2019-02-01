b12ae220008240f9d20fc7e9388ed369dc3b8fb8
Unknown <unknown@example.com>
2019-02-01 14:20:09 +0800

    测试

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -10,6 +10,6 @@ class TestController extends Controller
 {
     public function index(){
 //        return Carbon::now()->startOfDay()->toDateTimeString();
-        return 1233;
+        return 12332;
     }
 }
