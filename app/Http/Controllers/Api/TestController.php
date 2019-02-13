4ce025d9b523fb27b3d57418c5f37121724b15c6
Unknown <unknown@example.com>
2019-02-13 15:10:34 +0800

    打印sql

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class TestController extends Controller
 {
     public function index(){
+        //打印sql
+        DB::enableQueryLog();
+        $user = User::find(1);
+        $query = DB::getQueryLog();
+        var_dump($query);
 //        return Carbon::now()->startOfDay()->toDateTimeString();
         return '先大后小，思路为先，实验为辅。';
     }
